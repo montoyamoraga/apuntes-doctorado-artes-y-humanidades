5f907a70900fb4c273f54f134765147cf6d905b9
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-06-09 16:38:37 -0400

    agregar demo

--- semestre-01/traidores/software/main.py
+++ semestre-01/traidores/software/main.py
@@ -6,6 +6,9 @@ import json
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+
 data = None
 
 # abrir el archivo JSON
@@ -13,10 +16,10 @@ with open('data.json', 'r') as file:
     data = json.load(file)
 
 pygame.init()
-window = pygame.display.set_mode((800, 400))
+window = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-ft_font = pygame.freetype.SysFont('monospace', 30)
+ft_font = pygame.freetype.SysFont('monospace', 20)
 
 background = pygame.Surface(window.get_size())
 pygame.draw.rect(background, BLACK, background.get_rect())
@@ -32,7 +35,17 @@ while run:
 
     ft_font.render_to(window, (100, 100), 'administrador de traiciones', WHITE)
 
-    ft_font.render_to(window, (100, 200), 'traicion-01', WHITE)
+    ft_font.render_to(window, (100, 150), 'traicion-01', WHITE)
+
+    # print(data['traiciones'][0])
+    
+    iterator = 0
+
+    for i in data['traiciones'][0]:
+        iterator = iterator + 1
+        # print(i, data['traiciones'][0][i])
+        ft_font.render_to(window, (100, 200 + iterator*50), i, MAGENTA)
+        ft_font.render_to(window, (150, 220 + iterator*50), data['traiciones'][0][i], GREEN)
 
     
     pygame.display.flip()

610e03b6cc2146b8783328dbaa7b3d9c316734bf
montoyamoraga <montoyamoraga@gmail.com>
2025-06-15 23:01:02 -0400

    mover codigo de main a Administrador

--- semestre-01/traidores/software/Administrador.py
+++ semestre-01/traidores/software/Administrador.py
@@ -1,5 +1,17 @@
 # importar modulos Python
 import json
+import sys
+
+# importar modulos externos
+import pygame
+import pygame.freetype
+
+# importar modulos creados
+from constantes import ANCHO, ALTURA
+from constantes import NEGRO
+from constantes import ESTADO_AYUDA, ESTADO_DOCUMENTAR
+from constantes import ESTADO_INICIO, ESTADO_NAVEGAR
+import constantes
 
 
 class Administrador():
@@ -8,6 +20,140 @@ class Administrador():
         self.name = "administrador"
         self.data = None
         self.traicionActiva = 0
+        self.estado = ESTADO_INICIO
+        # iniciar pygame
+        pygame.init()
+        pygame.display.set_caption(constantes.TITULO)
+        self.ventana = pygame.display.set_mode((ANCHO, ALTURA))
+        self.reloj = pygame.time.Clock()
+        self.ft_font = pygame.freetype.SysFont('monospace', 20)
+        self.background = pygame.Surface(self.ventana.get_size())
+        pygame.draw.rect(self.background, NEGRO, self.background.get_rect())
         # abrir el archivo JSON
         with open('data.json', 'r') as file:
             self.data = json.load(file)
+
+    def detectarTecla(self, event, estado):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                print('presionaste a')
+                return ESTADO_AYUDA
+            if event.key == pygame.K_i:
+                print('presionaste i')
+                return ESTADO_INICIO
+            elif event.key == pygame.K_n:
+                print('presionaste n')
+                return ESTADO_NAVEGAR
+            elif event.key == pygame.K_d:
+                print('presionaste d')
+                return ESTADO_DOCUMENTAR
+            elif event.key == pygame.K_LEFT:
+                self.traicionActiva = self.traicionActiva - 1
+                if self.traicionActiva < 0:
+                    self.traicionActiva = 0
+                return estado
+            elif event.key == pygame.K_RIGHT:
+                self.traicionActiva = self.traicionActiva + 1
+                if self.traicionActiva >= len(self.data['traiciones']):
+                    self.traicionActiva = len(self.data['traiciones']) - 1
+                return estado
+        else:
+            return estado
+
+    def mostrarTitulo(self, titulo):
+        self.ft_font.render_to(
+            self.ventana,
+            (100, 100),
+            titulo,
+            constantes.BLANCO)
+
+    def mostrarSubTitulo(self, subtitulo):
+        self.ft_font.render_to(
+            self.ventana,
+            (100, 200),
+            subtitulo,
+            constantes.CYAN)
+
+    def mostrarTexto(self, texto, posY):
+        self.ft_font.render_to(
+            self.ventana,
+            (100, posY),
+            texto,
+            constantes.VERDE)
+
+    def mostrarCampoNombre(self, texto, posY):
+        self.ft_font.render_to(
+            self.ventana,
+            (100, posY),
+            texto,
+            constantes.VERDE)
+
+    def mostrarCampoValor(self, i, posY):
+        self.ft_font.render_to(
+            self.ventana,
+            (400, posY),
+            self.data['traiciones'][self.traicionActiva][i],
+            constantes.MAGENTA)
+
+    def mostrarCampoNombreValor(self, i, posY):
+        self.mostrarCampoNombre(i, posY)
+        self.mostrarCampoValor(i, posY)
+
+    def mostrarAyuda(self):
+        self.ventana.blit(self.background, (0, 0))
+        self.mostrarTitulo('administrador de traiciones')
+        self.mostrarSubTitulo(constantes.ESTADO_AYUDA)
+        self.mostrarTexto('a: ayuda', 300)
+        self.mostrarTexto('d: ayuda', 400)
+        self.mostrarTexto('i: inicio', 500)
+        self.mostrarTexto('n: ayuda', 600)
+
+    def mostrarDocumentar(self):
+        self.ventana.blit(self.background, (0, 0))
+        self.mostrarTitulo('administrador de traiciones')
+        self.mostrarSubTitulo(constantes.ESTADO_DOCUMENTAR)
+
+    def mostrarInicio(self):
+        self.ventana.blit(self.background, (0, 0))
+        self. mostrarTitulo('administrador de traiciones')
+        self.mostrarSubTitulo(constantes.ESTADO_INICIO)
+        self.mostrarTexto('v0.0.1 junio 2025', 300)
+        self.mostrarTexto('por montoyamoraga', 400)
+        self.mostrarTexto(
+            'para el curso traidores del profesor josé santos', 500)
+        self.mostrarTexto('en el doctorado de artes y humanidades', 600)
+        self.mostrarTexto('en idea usach', 700)
+
+    def mostrarNavegar(self):
+        self.ventana.blit(self.background, (0, 0))
+        self.mostrarTitulo('administrador de traiciones')
+        self.mostrarSubTitulo(constantes.ESTADO_NAVEGAR)
+        iterador = 0
+        for i in self.data['traiciones'][self.traicionActiva]:
+            iterador = iterador + 1
+            self.mostrarCampoNombreValor(i, 300 + iterador * 50)
+
+    def correr(self):
+        self.reloj.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif self.estado == constantes.ESTADO_AYUDA:
+                self.mostrarAyuda()
+                self.estado = self.detectarTecla(event, self.estado)
+            elif self.estado == constantes.ESTADO_DOCUMENTAR:
+                self.mostrarDocumentar()
+                self.estado = self.detectarTecla(event, self.estado)
+            elif self.estado == constantes.ESTADO_INICIO:
+                self.mostrarInicio()
+                self.estado = self.detectarTecla(event, self.estado)
+            elif self.estado == constantes.ESTADO_NAVEGAR:
+                self.mostrarNavegar()
+                self.estado = self.detectarTecla(event, self.estado)
+            else:
+                self.estado = self.detectarTecla(event, self.estado)
+                print("whatever")
+
+        pygame.display.flip()
--- semestre-01/traidores/software/constantes.py
+++ semestre-01/traidores/software/constantes.py
@@ -5,9 +5,16 @@ VERDE = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 
-# definir constantes para pygame
+# constantes para pygame
 ANCHO = 800
 ALTURA = 800
 ALTURA_PANEL = 60
 ALTURA_USABLE = ALTURA - ALTURA_PANEL
 CUADROS_POR_SEGUNDO = 30
+TITULO = 'administrador de traiciones'
+
+# constantes para los estados
+ESTADO_INICIO = 'inicio'
+ESTADO_NAVEGAR = 'navegar'
+ESTADO_DOCUMENTAR = 'documentar'
+ESTADO_AYUDA = 'ayuda'

3de210969f84be7884dd6f62cb7f66500e9aaeee
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-11-17 15:09:09 -0300

    agregar tarea

--- semestre-02/traduccion-intersemiotica/clase-13/README.md
+++ semestre-02/traduccion-intersemiotica/clase-13/README.md
@@ -1,3 +1,7 @@
 # clase-13
 
 lunes 17 noviembre 2025
+
+## tarea: glossolalia
+
+para esto escribo un script en python que llame al comando say de macOS para leer en voz alta un texto.
---
+++ semestre-02/traduccion-intersemiotica/clase-13/glossolalia.py
@@ -0,0 +1,35 @@
+# curso traduccion intersemiotica
+# dictado por profesora doctora jèssica pujol
+# tarea por aaron montoya
+# lunes 17 noviembre 2025
+
+import random
+import os
+
+
+alfabeto = "abcdefghijklmnopqrstuvwxyzñ"
+
+
+# crear una silaba aleatoria entre 1 y 3 caracteres
+def silabaAleatoria():
+    largo = random.randint(1, 3)
+    return ''.join(random.choice(alfabeto) for x in range(largo))
+
+
+# funcion que usa comando say de macOS
+# para leer texto en cierta voz y cierta velocidad
+def glossolalia(text, voice="Francisca", velocidad=200):
+    command = f'say -v {voice} -r {velocidad} "{text}"'
+    os.system(command)
+
+
+# uso
+try:
+    while True:
+        silabas = [silabaAleatoria() for x in range(10)]
+        texto = ''.join(silabas)
+        print(texto)
+        velocidad = random.randint(150, 300)
+        glossolalia(texto, "Francisca", velocidad)
+except KeyboardInterrupt:
+    print("\fin de la glossolalia")
